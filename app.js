1bd28b039ca253f587472d673a9df7f7e6794f55
jonathan <jnmorales73@live.com>
2021-08-07 22:06:21 -0500

    jj

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-// Variables
+
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
 const listaCursos = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
-
-
-// Listeners
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito')
 
 
 function cargarEventListeners() {
-    // Dispara cuando se presiona "Agregar Carrito"
     cursos.addEventListener('click', comprarCurso);
-    // Cuando se elimina un curso del carrito
     carrito.addEventListener('click', eliminarCurso);
-    // Al Vaciar el carrito
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-    // Al cargar el documento, mostrar LocalStorage
     document.addEventListener('DOMContentLoaded', leerLocalStorage);
 }
 cargarEventListeners();
 
-// Funciones
-// Función que añade el curso al carrito
 function comprarCurso(e) {
     e.preventDefault();
-    // Delegation para agregar-carrito
     if (e.target.classList.contains('agregar-carrito')) {
         const curso = e.target.parentElement.parentElement;
-        // Enviamos el curso seleccionado para tomar sus datos
         leerDatosCurso(curso);
     }
 }
 
-// Lee los datos del curso
 function leerDatosCurso(curso) {
     const infoCurso = {
         imagen: curso.querySelector('img').src,
@@ -43,7 +31,6 @@ function leerDatosCurso(curso) {
     insertarCarrito(infoCurso);
 }
 
-// Muestra el curso seleccionado en el Carrito
 function insertarCarrito(curso) {
     const row = document.createElement('tr');
     row.innerHTML = `
@@ -60,7 +47,6 @@ function insertarCarrito(curso) {
     guardarCursoLocalStorage(curso);
 }
 
-// Elimina el curso del carrito en el DOM
 function eliminarCurso(e) {
     e.preventDefault();
     let curso,
@@ -73,34 +59,31 @@ function eliminarCurso(e) {
     eliminarCursoLocalStorage(cursoId);
 }
 
-// Elimina los cursos del carrito en el DOM
+
 function vaciarCarrito() {
-    // forma lenta
-    // listaCursos.innerHTML = '';
-    // forma rapida (recomendada)
+  
     while (listaCursos.firstChild) {
         listaCursos.removeChild(listaCursos.firstChild);
     }
 
-    // Vaciar Local Storage
+    
     vaciarLocalStorage();
     return false;
 }
 
-// Almacena cursos en el carrito a Local Storage
+
 function guardarCursoLocalStorage(curso) {
     let cursos;
-    // Toma el valor de un arreglo con datos de LS o vacio
+    
     cursos = obtenerCursosLocalStorage();
-    // el curso seleccionado se agrega al arreglo
     cursos.push(curso);
     localStorage.setItem('cursos', JSON.stringify(cursos));
 }
 
-// Comprueba que haya elementos en Local Storage
+
 function obtenerCursosLocalStorage() {
     let cursosLS;
-    // comprobamos si hay algo en localStorage
+    
     if (localStorage.getItem('cursos') === null) {
         cursosLS = [];
     } else {
@@ -109,12 +92,12 @@ function obtenerCursosLocalStorage() {
     return cursosLS;
 }
 
-// Imprime los cursos de Local Storage en el carrito
+
 function leerLocalStorage() {
     let cursosLS;
     cursosLS = obtenerCursosLocalStorage();
     cursosLS.forEach(function(curso) {
-        // constrir el template
+        
         const row = document.createElement('tr');
         row.innerHTML = `
   <td>
@@ -130,22 +113,19 @@ function leerLocalStorage() {
     });
 }
 
-// Elimina el curso por el ID en Local Storage
+
 function eliminarCursoLocalStorage(curso) {
     let cursosLS;
-    // Obtenemos el arreglo de cursos
     cursosLS = obtenerCursosLocalStorage();
-    // Iteramos comparando el ID del curso borrado con los del LS
     cursosLS.forEach(function(cursoLS, index) {
         if (cursoLS.id === curso) {
             cursosLS.splice(index, 1);
         }
     });
-    // Añadimos el arreglo actual a storage
     localStorage.setItem('cursos', JSON.stringify(cursosLS));
 }
 
-// Elimina todos los cursos de Local Storage
+
 function vaciarLocalStorage() {
     localStorage.clear();
 };
